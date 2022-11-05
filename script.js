65d33ab980ebbb2100c72a6a6ec8ad6a25537efe
corinavera <111809833+corinavera@users.noreply.github.com>
2022-11-04 20:01:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+/* Here are some functions to move the content forward and back */
 
-console.log("Hello, world!");
+let counter = 1;
+
+const goForward = () => {
+  if (counter < 3) {
+    counter++;
+    let idToMatch = "#slide-" + counter;
+    document.querySelector(idToMatch).classList.remove("hidden");
+  }
+};
+
+const goBack = () => {
+  if (counter > 1) {
+    let idToMatch = "#slide-" + counter;
+    document.querySelector(idToMatch).classList.add("hidden");
+    counter--;
+  }
+};
+
+const downloadChecklist = () => {
+  let contentToPrint = document.querySelector("#checklist");
+
+  html2canvas(contentToPrint, {}).then((canvas) => {
+    let a = document.createElement("a");
+    a.download = "checklist.png";
+    a.href = canvas.toDataURL("image/png");
+    a.click();
+  });
+};
+
+const toggleChecklist = () => {
+  let myChecklist = document.querySelector("#checklist");
+  myChecklist.classList.toggle("hidden");
+
+  let toggleButton = document.querySelector("#toggle-checklist");
+
+  if (myChecklist.classList.contains("hidden")) {
+    toggleButton.innerHTML = "Open Checklist";
+  } else {
+    toggleButton.innerHTML = "Close Checklist";
+  }
+};
+
